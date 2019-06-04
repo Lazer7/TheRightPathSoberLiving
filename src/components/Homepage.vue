e6ec25add264dbdeedd82dfdfa23d5df3bc842fb
Jimmy <iamjimmyjimmy@yahoo.com>
2019-06-03 23:31:12 -0700

    Homepage Setup
    Creating a work in progress homepage

---
+++ src/components/Homepage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="../assets/logo.png"/><br/>
+    <h1>THE RIGHT PATH SOBER LIVING WORK IN PROGRESS!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"homepage"
+}
+</script>
+
+<style>
+
+</style>
